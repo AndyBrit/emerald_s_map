25daee55da3f002325629f2dc66458c7a7705605
AndyBrit <118849877+AndyBrit@users.noreply.github.com>
2022-11-23 01:57:27 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -417,13 +417,13 @@ var baseLayers = {
 };
 
 var groupedOverlays = {
-  "Points of Interest": {
-    "<img src='assets/img/theater.png' width='24' height='28'>&nbsp;Theaters": theaterLayer,
-    "<img src='assets/img/museum.png' width='24' height='28'>&nbsp;Museums": museumLayer
+  "Локації": {
+    "<img src='assets/img/Damage.png' width='24' height='28'>&nbsp;Точки екошкоди": theaterLayer,
+    /* "<img src='assets/img/museum.png' width='24' height='28'>&nbsp;Museums": museumLayer */
   },
-  "Reference": {
-    "Boroughs": boroughs,
-    "Subway Lines": subwayLines
+  "Межі смарагдових зон": {
+    "Зони": boroughs,
+  /*  "Subway Lines": subwayLines */
   }
 };
 
